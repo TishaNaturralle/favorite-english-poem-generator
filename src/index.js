c91f47c6dc6e0bc9dd90c49476873379301b6f0e
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2025-02-22 21:41:20 +0200

    Added basic form handling and added Typewriter.js plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Welocome to AI English Poem Generator");
+function generatePoem (event) {
+    event.preventDefault();
+
+new Typewriter("#poem", {
+    strings: "The Road Not Taken",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+  
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
