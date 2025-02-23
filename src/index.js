3af8e7ebc4addd78d55d4f37da1428af90c896ac
TishaNaturralle <138395666+TishaNaturralle@users.noreply.github.com>
2025-02-23 06:43:46 +0200

    Added the API intergration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-function generatePoem (event) {
-    event.preventDefault();
+//function which will get a response that is response.data.answer
+function displayPoem(response){
 
-new Typewriter("#poem", {
-    strings: "The Road Not Taken",
+  console.log("poem generated");
+  new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 1,
   });
 }
+
+function generatePoem (event) {
+    event.preventDefault();
+
+// build the API URL
+let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "dt7522ba4c017dfaoc53ab6bcb9a6246";
+let prompt =`User instructions: Generate an English poem about ${instructionsInput.value}`;
+let context ="You are an expert in english poems and love to write poems.Your mission is to generate a poem in basic HTML and do not include the word html before the poem starts. Include the poem title and author's name before the poem starts. Make sure to follow the user instructions, Sign with the words 'brought to you by  TichTach AI',inside a <strong> element at the end of the poem ";
+let apiURL =
+`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+//Make a call to the API
+
+console.log("Generating poem");
+console.log('Prompt: ${prompt}');
+console.log('Context: ${context}');
+
+
+axios.get(apiURL).then(displayPoem);
+//Display the generated poem
+
+
+
+}
   
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
+
